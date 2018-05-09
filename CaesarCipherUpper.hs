a8e0acafb27b4c5232b6c4a4f3739babfb87a889
Mike Gainey <mikeg9b@gmail.com>
2018-05-08 23:15:28 -0500

    successfully completed Caesar Cipher, chapter 5 #10
    - works with upper *and* lower case characters
    - reimplementation from memory
    - replaces list comprehensions with map & filter here and there

--- CaesarCipherUpper.hs
+++ CaesarCipherUpper.hs
@@ -1,11 +1,21 @@
 -- chapter 5, exercise 10: modify Caesar Cipher to handle upper-case characters
--- and this is a reimplementation from memory (attempt)
+-- and this is a reimplementation from memory (and it works!)
 import Data.Char -- for ord and chr
 
--- lowerString
+letters :: [Char]
+letters = ['A'..'Z'] ++ ['a'..'z']
 
--- TODO: count needs a string already converted to lowercase
-count character string = length $ filter (== character) string
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [ b | (a,b) <- zip xs [0..], a == x ]
+
+minpos :: Ord a => [a] -> Int
+minpos xs = head $ positions (minimum xs) xs
+
+lowerString :: String -> String
+lowerString string = map toLower string
+
+count :: Char -> String -> Int
+count character string = length $ filter (== character) (lowerString string)
 
 letter2asc :: Char -> Int
 letter2asc letter = ord letter
@@ -19,20 +29,31 @@ encodeLetter shift letter
   | isLower letter = chr ((((ord letter - 97) + shift) `mod` 26) + 97)
   | otherwise = letter
 
--- encode: maps shiftLetter
+encode :: Int -> String -> String
 encode shift string = map (encodeLetter shift) string
 
--- freqs
--- freqs string = map (\c -> 1) lowerString
---   where lowerString = map toLower string
---         lenString = length string
+percent :: Int -> Int -> Float
+percent n d = (fromIntegral n) / (fromIntegral d) * 100
 
-table :: [Float]
-table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0,
-         0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
-         6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+freqs :: String -> [Float]
+freqs string = map (\c -> percent (count c string) letterCount) ['a'..'z']
+  where letterCount = length $ filter (\c -> c `elem` letters) string
+
+normfreq :: [Float]
+normfreq = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0,
+            0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
+            6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
 
 chisqr :: [Float] -> [Float] -> Float
 chisqr os es = sum [ ((o - e)^2)/e | (o,e) <- zip os es ]
 
--- crack
+rotate :: Int -> [a] -> [a]
+rotate n xs = (drop n xs) ++ (take n xs)
+
+chitab :: String -> [Float]
+chitab string = [ chisqr (rotate r strfreq) normfreq | r <- [0..25] ]
+  where strfreq = freqs string
+
+crack :: String -> String
+crack string = encode (negate rotation) string
+  where rotation = minpos $ chitab string
