013aaac1f35260211429760ac43cd7e180599cd8
Mike Gainey <mikeg9b@gmail.com>
2018-05-29 22:50:13 -0500

    added my own versions of showRow and putGrid

--- ch11-tictactoe.hs
+++ ch11-tictactoe.hs
@@ -48,11 +48,20 @@ putGrid :: Grid -> IO ()
 putGrid = putStrLn . unlines . concat . interleave bar . map showRow
   where bar = [replicate ((size*4)-1) '-']
 
+myPutGrid :: Grid -> IO ()
+myPutGrid g = putStrLn $ unlines $ concat $ intersperse [line] $ map myShowRow g
+  where  line = replicate (size*3 + (size-1)) '-'
+
 showRow :: [Player] -> [String]
 showRow = beside . interleave bar . map showPlayer
   where beside = foldr1 (zipWith (++))
         bar = replicate 3 "|"
 
+myShowRow :: [Player] -> [String]
+myShowRow r = [blank,row,blank]
+  where row = concat $ intersperse "|" $ map (\x -> " " ++ show x ++ " ") r
+        blank = concat $ intersperse "|" $ replicate size "   "
+ 
 showPlayer :: Player -> [String]
 showPlayer O = ["   ", " O ", "   "]
 showPlayer B = ["   ", "   ", "   "]
@@ -62,5 +71,6 @@ interleave :: a -> [a] -> [a]
 interleave x [] = []
 interleave x [y] = [y]
 interleave x (y:ys) = y : x : interleave x ys
+
 
 
