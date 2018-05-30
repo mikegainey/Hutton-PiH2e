7f6df6c0a749208a5d757b71ad9eb558722e1fa7
Mike Gainey <mikeg9b@gmail.com>
2018-05-30 03:03:37 -0500

    finished 1-player version

--- ch11-tictactoe.hs
+++ ch11-tictactoe.hs
@@ -86,4 +86,38 @@ chop n xs = take n xs : chop n (drop n xs)
 getNat :: String -> IO Int
 getNat prompt = do putStr prompt
                    xs <- getLine
-                   if xs /= []
+                   if xs /= [] && all isDigit xs
+                     then return (read xs)
+                     else do putStrLn "ERROR: Invalid number"
+                             getNat prompt
+
+tictactoe :: IO ()
+tictactoe = run empty O -- O goes first
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int, Int)
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+run :: Grid -> Player -> IO ()
+run g p = do cls
+             goto (1,1)
+             putGrid g
+             run' g p
+
+run' :: Grid -> Player -> IO ()
+run' g p
+  | wins O g  = putStrLn "Player O wins!\n"
+  | wins X g  = putStrLn "Player X wins!\n"
+  | full g    = putStrLn "It's a draw!\n"
+  | otherwise = do i <- getNat (prompt p)
+                   case move g i p of
+                     []   -> do putStrLn "ERROR: Invalid move!"
+                                run' g p
+                     [g'] -> run g' (next p)
+
+prompt :: Player -> String
+prompt p = "Player " ++ show p ++ ", enter your move: "
